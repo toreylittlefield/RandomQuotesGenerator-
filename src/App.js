5e9fe6bcda105671cf9b7445515dbe4c6d41ce49
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-12-24 08:10:00 +0800

    add twitter auth provider using usecontext to save login state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import Header from "./Header/Header";
-import QuotesMain from "./QuoteDivBlock/QuotesMain";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-
-
-
+import Header from './Header/Header';
+import QuotesMain from './QuoteDivBlock/QuotesMain';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TwitterAuthProvider from './Auth/TwitterAuth';
 
 function App() {
-
-
   return (
+    <TwitterAuthProvider>
+      <Router>
+        <Header />
 
-    <Router>
-      <Header />
-
-      <QuotesMain />
-
-
-    </Router>
-
-
-
+        <QuotesMain />
+      </Router>
+    </TwitterAuthProvider>
   );
 }
 
