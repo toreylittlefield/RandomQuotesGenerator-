72a31103c65d6056fbb795c53ddcc02100d750ff
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-12-26 21:11:05 +0800

    run prettier

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import Header from './Header/Header';
 import QuotesMain from './QuoteDivBlock/QuotesMain';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
@@ -8,28 +7,28 @@ import TwitterAuthProvider from './Auth/TwitterAuth';
 
 function App() {
   return (
-<TwitterAuthProvider>
-    <Router>
-      <Header />
-      <ul className="headerNav">
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/Search">Search Quotes</NavLink>
-        </li>
-      </ul>
+    <TwitterAuthProvider>
+      <Router>
+        <Header />
+        <ul className="headerNav">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Search">Search Quotes</NavLink>
+          </li>
+        </ul>
 
-      <Switch>
-        <Route path="/Search">
-          <SearchPage />
-        </Route>
-        <Route path="/">
-          <QuotesMain />
-        </Route>
-      </Switch>
-    </Router>
-</TwitterAuthProvider>
+        <Switch>
+          <Route path="/Search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <QuotesMain />
+          </Route>
+        </Switch>
+      </Router>
+    </TwitterAuthProvider>
   );
 }
 
