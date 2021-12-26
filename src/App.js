2f56a33c82503fd74ee9af4769e4b0efd6b2a793
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-12-26 20:58:20 +0800

    install update gitignore

--- src/App.js
+++ src/App.js
@@ -1,41 +1,31 @@
-import Header from "./Header/Header";
-import QuotesMain from "./QuoteDivBlock/QuotesMain";
-import { BrowserRouter as Router, Route, Switch, NavLink } from "react-router-dom";
-import SearchPage from "./SearchPage/SearchPage";
-
-
-
+import Header from './Header/Header';
+import QuotesMain from './QuoteDivBlock/QuotesMain';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { Switch } from 'react-router';
+import SearchPage from './SearchPage/SearchPage';
 
 function App() {
-
-
   return (
-
     <Router>
       <Header />
       <ul className="headerNav">
-        <li><NavLink to='/'>Home</NavLink></li>
-        <li><NavLink to='/Search'>Search Quotes</NavLink></li>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/Search">Search Quotes</NavLink>
+        </li>
       </ul>
 
       <Switch>
-        <Route path='/Search'>
+        <Route path="/Search">
           <SearchPage />
         </Route>
-        <Route path='/'>
+        <Route path="/">
           <QuotesMain />
-
         </Route>
-
-
       </Switch>
-
-
-
     </Router>
-
-
-
   );
 }
 
