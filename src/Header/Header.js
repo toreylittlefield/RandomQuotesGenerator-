3c9c645bc74f214cc609fe1ade9c5b94b17ca670
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-12-26 23:28:35 +0800

    add twitter login buttons

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,25 +1,26 @@
-
-
+import { useContext } from 'react';
+import { UserContext } from '../Auth/TwitterAuth';
 
 const Header = () => {
-    //Add SVG into the searchbar
-    // const returnSVG = () => {
-    //     return (
-    //         <img src="https://img.icons8.com/office/16/000000/search--v2.png" />
-    //     )
-    // }
+  const context = useContext(UserContext);
+  //Add SVG into the searchbar
+  // const returnSVG = () => {
+  //     return (
+  //         <img src="https://img.icons8.com/office/16/000000/search--v2.png" />
+  //     )
+  // }
 
-    return (
-        <nav>
-            <div className="headerDiv">
-                <h1>Macs <span style={{ color: 'grey', fontStyle: 'italic' }}>Quote</span>  Generator</h1>
-
-            </div>
-
-        </nav>
-
-
-    )
-}
+  return (
+    <nav>
+      <div className="headerDiv">
+        <h1>
+          Macs <span style={{ color: 'grey', fontStyle: 'italic' }}>Quote</span> Generator
+        </h1>
+        {!context.auth && !context.token && <a href="/.netlify/functions/twitterauth">Sign Into Twitter</a>}
+        {context.auth && context.token && <a href="/">Sign Out</a>}
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
