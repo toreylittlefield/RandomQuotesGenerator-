dc66bc3fdc32d614052ea921ab46884cc69173a4
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-12-28 23:36:05 +0800

    update scopes & redirect uri

--- netlify/functions/twitterauth.js
+++ netlify/functions/twitterauth.js
@@ -1,7 +1,7 @@
 exports.handler = async (event, context) => {
   try {
     const CLIENT_ID = process.env.REACT_APP_TWITTER_CLIENT_ID;
-    const url = `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=http://localhost:3000&scope=tweet.read&state=state&code_challenge=challenge&code_challenge_method=plain`;
+    const url = `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=http://localhost:8888/.netlify/functions/twitterauthcallback&scope=tweet.read%20users.read%20follows.read&state=state&code_challenge=challenge&code_challenge_method=plain`;
     return {
       statusCode: 302,
       headers: {
