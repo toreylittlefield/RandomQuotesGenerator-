09b41fab74c4f637e9b7eb14ca55ba82aaadd449
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-12-29 12:24:31 +0800

    add scopes

--- netlify/functions/utils/auth-config.js
+++ netlify/functions/utils/auth-config.js
@@ -1,5 +1,23 @@
 const sourceURL = process.env.NODE_ENV === 'production' ? process.env.SITE_URL : 'http://localhost:8888';
 
+const availableScopes = {
+  tweetRead: 'tweet.read',
+  tweetWrite: 'tweet.write',
+  tweetModerateWrite: 'tweet.moderate.write',
+  usersRead: 'users.read',
+  likeRead: 'like.read',
+  likeWrite: 'like.write',
+  followsRead: 'follows.read',
+  followsWrite: 'follows.write',
+  blockRead: 'block.read',
+  blockWrite: 'block.write',
+  muteRead: 'mute.read',
+  muteWrite: 'mute.write',
+  spaceRead: 'space.read',
+  listRead: 'list.read',
+  listWrite: 'list.write',
+};
+
 const config = {
   code: '',
   clientId: process.env.REACT_APP_TWITTER_CLIENT_ID,
@@ -8,6 +26,4 @@ const config = {
   codeVerifier: 'challenge',
 };
 
-const authTokens = { accessToken: '', refreshToken: '' };
-
-module.exports = { sourceURL, config, authTokens };
+module.exports = { sourceURL, config, availableScopes };
