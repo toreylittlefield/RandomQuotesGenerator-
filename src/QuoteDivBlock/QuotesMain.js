c76ff5dc46e2a77df3c4b8780aac812c6028180f
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-12-29 17:55:04 +0800

    format and useCallback and add deps

--- src/QuoteDivBlock/QuotesMain.js
+++ src/QuoteDivBlock/QuotesMain.js
@@ -1,75 +1,59 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from 'react';
 
-import ButtonNext from "./ButtonNext";
-import ButtonShare from "./ButtonShare.js";
-import Quotes from "./Quotes";
+import ButtonNext from './ButtonNext';
+import ButtonShare from './ButtonShare.js';
+import Quotes from './Quotes';
 // import TrialButton from "./tweetPost";
 // import TrialButton from "./tweetPost";
-
-
-
-
 
 const QuotesMain = () => {
-    const [currentQuote, setCurrentQuote] = useState('');
-    const [currentBgColor, setCurrentBgColor] = useState('');
+  const [currentQuote, setCurrentQuote] = useState('');
+  const [currentBgColor, setCurrentBgColor] = useState('');
 
+  function randomGenerator() {
+    const bgColor = `rgb(${Math.floor(Math.random() * 255) + 100}, ${Math.floor(Math.random() * 255) + 100}, ${
+      Math.floor(Math.random() * 255) + 100
+    })`;
 
+    setCurrentBgColor(bgColor);
+  }
 
-
-
-    function randomGenerator() {
-        const bgColor = `rgb(${Math.floor(Math.random() * 255) + 100}, ${Math.floor(Math.random() * 255) + 100}, ${Math.floor(Math.random() * 255) + 100})`
-
-        setCurrentBgColor(bgColor)
-    }
-
-
-
-    const fetchQuotes = async () => {
-        try {
-            const response = await fetch('https://api.quotable.io/random');
-            if (response.ok) {
-                const jsonResponse = await response.json()
-                console.log(jsonResponse)
-                randomGenerator()
-                setCurrentQuote(jsonResponse)
-                return jsonResponse;
-            }
-            throw new Error('Error')
-
-        } catch (e) {
-            console.log(e)
+  const fetchQuotes = useCallback(
+    () => async () => {
+      try {
+        const response = await fetch('https://api.quotable.io/random');
+        if (response.ok) {
+          const jsonResponse = await response.json();
+          randomGenerator();
+          setCurrentQuote(jsonResponse);
+          return jsonResponse;
         }
+        throw new Error('Error');
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    []
+  );
 
-    }
+  useEffect(() => {
+    fetchQuotes();
+  }, [fetchQuotes]);
 
-    useEffect(() => {
-
-
-        fetchQuotes()
-    }, [])
-
-
-
-
-    return (
-        <div className="quotesMain" style={{ backgroundColor: currentBgColor }} id="quote-box">
-
-            <div>
-                <Quotes quote={currentQuote} />
-            </div>
-            <div>
-                {/* <ButtonShare id="tweet-quote" shareTweet={currentQuote} /> */}
-                <ButtonNext onClick={fetchQuotes} id="new-quote" />
-                {/* <TrialButton /> */}
-                {/* <TrialButton /> */}
-                {/* <TrialButton /> */}
-
-            </div>
-
-        </div>
-    )
-}
+  return (
+    <div className="quotesMain" style={{ backgroundColor: currentBgColor }} id="quote-box">
+      <div>
+        <Quotes quote={currentQuote} />
+      </div>
+      <div>
+        <ButtonShare id="tweet-quote" shareTweet={currentQuote} />
+        <ButtonNext onClick={fetchQuotes} id="new-quote" />
+        {/* <TrialButton /> */}
+        {/* <TrialButton /> */}
+        {/* <TrialButton /> */}
+      </div>
+    </div>
+  );
+};
 
 export default QuotesMain;
