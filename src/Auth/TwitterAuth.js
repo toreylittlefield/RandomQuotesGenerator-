099e4a014436f7785873259faa9fb8f0ea80611d
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-12-29 21:01:16 +0800

    add extra user info to useContext and set initial state

--- src/Auth/TwitterAuth.js
+++ src/Auth/TwitterAuth.js
@@ -1,39 +1,33 @@
 import React, { createContext, useState } from 'react';
 
+const initialState = { auth: false, name: '', token: '', expires: '', id: '', username: '', profile_image_url: '' };
+
 export const UserContext = createContext({
-  user: {
-    auth: false,
-    name: '',
-    token: '',
-    expires: '',
-  },
-  login: ({ name = '', token = '', expires = '' }) => {},
+  user: initialState,
+  login: ({ name = '', token = '', expires = '', id = '', username = '', profile_image_url = '' }) => {},
   logout: () => {},
 });
 
 const TwitterAuthProvider = ({ children }) => {
   // User is the name of the "data" that gets stored in context
-  const [user, setUser] = useState({ auth: false, name: '', token: '', expires: '' });
+  const [user, setUser] = useState(initialState);
 
   // Login updates the user data with a name parameter
-  const login = ({ name = '', token = '', expires = '' }) => {
-    console.log({ name, token, expires, user });
+  const login = ({ name = '', token = '', expires = '', id = '', username = '', profile_image_url = '' }) => {
     setUser((_) => ({
       expires,
       token,
       name,
+      id,
+      username,
+      profile_image_url,
       auth: true,
     }));
   };
 
   // Logout updates the user data to default
-  const logout = ({ name = '', token = '', expires = '' }) => {
-    setUser((_) => ({
-      expires,
-      token,
-      name,
-      auth: false,
-    }));
+  const logout = () => {
+    setUser(initialState);
   };
 
   return <UserContext.Provider value={{ user, login, logout }}>{children}</UserContext.Provider>;
