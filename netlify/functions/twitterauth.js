1b8eb6c25ae22bac0666fd7449e40c7011ebd694
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-12-29 12:24:10 +0800

    add scopes for twitter permissions

--- netlify/functions/twitterauth.js
+++ netlify/functions/twitterauth.js
@@ -1,7 +1,16 @@
+const { sourceURL, config, availableScopes } = require('./utils/auth-config');
+
 exports.handler = async (event, context) => {
   try {
-    const CLIENT_ID = process.env.REACT_APP_TWITTER_CLIENT_ID;
-    const url = `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=http://localhost:8888/.netlify/functions/twitterauthcallback&scope=tweet.read%20users.read%20follows.read&state=state&code_challenge=challenge&code_challenge_method=plain`;
+    const scopes = Object.keys(availableScopes)
+      // .slice(0, 4)
+      .map((key) => {
+        return encodeURIComponent(availableScopes[key]);
+      })
+      .join('%20');
+    console.log({ scopes }, 'scopes');
+    // const url = `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=${config.clientId}&redirect_uri=${sourceURL}/.netlify/functions/twitterauthcallback&scope=tweet.read%20users.read%20follows.read&state=state&code_challenge=challenge&code_challenge_method=plain`;
+    const url = `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=${config.clientId}&redirect_uri=${sourceURL}/.netlify/functions/twitterauthcallback&scope=${scopes}&state=state&code_challenge=challenge&code_challenge_method=plain`;
     return {
       statusCode: 302,
       headers: {
