c55d0ca330c4dcf157a1c3408c66fd6ff6a71c83
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-12-29 21:01:28 +0800

    refactor

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,48 +1,51 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { UserContext } from '../Auth/TwitterAuth';
 import { parseHash, removeHash } from '../Utils/auth';
 import { useEffect } from 'react';
 
 const Header = () => {
   const {
-    user: { auth, token },
+    user: { auth, token, id, name, profile_image_url, username, expires },
     login,
+    logout,
   } = useContext(UserContext);
   const response = parseHash(window.location.hash);
 
   /* Clear hash */
   removeHash();
+
   //Add SVG into the searchbar
   // const returnSVG = () => {
   //     return (
   //         <img src="https://img.icons8.com/office/16/000000/search--v2.png" />
   //     )
   // }
-  const [state, setstate] = useState(null);
 
+  // get twitter user data once we have a token from the twitterauthcallback
   useEffect(() => {
-    if (!token && auth === false) return;
+    if (auth === true || !response.token) return;
     (async () => {
-      const res = await fetch('/.netlify/functions/getuserinfo', {
-        method: 'POST',
-        body: JSON.stringify(token),
-      });
-      const json = await res.json();
-      console.log(json);
-      setstate(json);
+      const token = window.atob(response.token);
+      const expires = response.expires_in;
+      try {
+        const res = await fetch('/.netlify/functions/getuserinfo', {
+          method: 'POST',
+          body: JSON.stringify({ token }),
+        });
+        const json = await res.json();
+        const { username, profile_image_url, id, name } = json.data;
+        login({ token, expires, username, profile_image_url, id, name });
+      } catch (error) {
+        console.error(error);
+        login({ token, expires });
+      }
     })();
-    // return () => {
-    //   cleanup
-    // }
-  }, [token, auth]);
+  }, [response, login, auth]);
 
-  useEffect(() => {
-    if (response.token && auth === false) {
-      const access_token = window.atob(response.token);
-      const token_expiration = response.expires;
-      login({ token: access_token, expires: token_expiration });
-    }
-  }, [response.token, login, response.expires, auth]);
+  const handleSignOut = (e) => {
+    e.preventDefault();
+    logout();
+  };
 
   return (
     <nav>
@@ -50,12 +53,16 @@ const Header = () => {
         <h1>
           Macs <span style={{ color: 'grey', fontStyle: 'italic' }}>Quote</span> Generator
         </h1>
-        {!auth && !token && <a href="/.netlify/functions/twitterauth">Sign Into Twitter</a>}
-        {auth && token && <a href="/">Sign Out</a>}
-        {state && (
+        {!auth && !token && (
+          <a href="/.netlify/functions/twitterauth">
+            <button type="button">Sign Into Twitter</button>
+          </a>
+        )}
+        {auth && token && <button onClick={handleSignOut}>Sign Out</button>}
+        {profile_image_url && (
           <div>
-            {JSON.stringify(state, null, 2)}
-            <img src={state.data.profile_image_url} alt={state.data.username} width={45} height={45} />
+            {JSON.stringify({ auth, token, id, name, profile_image_url, username, expires }, null, 2)}
+            <img src={profile_image_url} alt={username} width={45} height={45} />
           </div>
         )}
       </div>
