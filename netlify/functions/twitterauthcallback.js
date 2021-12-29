d9e29bc988d1cf95c1592b4225e1c929e7daeaae
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-12-29 09:39:40 +0800

    refactor

--- netlify/functions/twitterauthcallback.js
+++ netlify/functions/twitterauthcallback.js
@@ -1,8 +1,11 @@
+import { config, sourceURL, authTokens } from './utils/auth-config';
+import { getAuthToken } from './utils/get-auth-token';
 import fetch from 'node-fetch';
 
 exports.handler = async (event, context) => {
   try {
     const code = event.queryStringParameters.code;
+    // no code user is unauthorized
     if (!code) {
       return {
         statusCode: 401,
@@ -10,46 +13,27 @@ exports.handler = async (event, context) => {
         body: JSON.stringify({ message: 'Unauthorized' }),
       };
     }
-    const CLIENT_ID = process.env.REACT_APP_TWITTER_CLIENT_ID;
-    const params = {
-      code: code,
-      grant_type: 'authorization_code',
-      client_id: CLIENT_ID,
-      redirect_uri: 'http://localhost:8888/.netlify/functions/twitterauthcallback',
-      code_verifier: 'challenge',
-    };
-    const searchParams = Object.keys(params)
-      .map((key) => {
-        return encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
-      })
-      .join('&');
+    // add code from twitter & exchange for access token
+    const tokenRes = await getAuthToken(config, code);
 
-    const twitterUrl = `https://api.twitter.com/2/oauth2/token`;
-    const twitterRes = await fetch(twitterUrl, {
+    //
+    console.table(tokenRes);
+    authTokens.accessToken = tokenRes.access_token;
+    console.table(authTokens);
+    const getTweets = await fetch(`${sourceURL}/.netlify/functions/twitter_read_tweets`, {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: searchParams,
+      body: JSON.stringify(authTokens),
     });
-    if (twitterRes.ok) {
-      const json = await twitterRes.json();
-      console.log({ json, res: twitterRes.statusText });
-      const getTweets = await fetch('http://localhost:8888/.netlify/functions/twitter_read_tweets', {
-        method: 'POST',
-        body: JSON.stringify(json),
-      });
-      const jsonData = await getTweets.json();
-      console.log(jsonData);
-      return {
-        statusCode: 302,
-        headers: {
-          Location: `http://localhost:8888?${JSON.stringify(json)}`,
-        },
-        'Cache-Control': 'no-cache',
-        body: null,
-      };
-    }
+    const jsonData = await getTweets.json();
+    console.log(jsonData);
+    return {
+      statusCode: 302,
+      headers: {
+        Location: `${sourceURL}/?${JSON.stringify(tokenRes)}`,
+      },
+      'Cache-Control': 'no-cache',
+      body: null,
+    };
   } catch (error) {
     console.error(error);
     return {
