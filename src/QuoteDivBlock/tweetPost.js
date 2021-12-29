2f60ab7589346b6b99e2297e03e2f5ebe4f596a7
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-12-29 22:29:28 +0800

    the trial button component (should be renamed)

--- src/QuoteDivBlock/tweetPost.js
+++ src/QuoteDivBlock/tweetPost.js
@@ -1,76 +1,75 @@
-import { useEffect, useState } from 'react';
+import { useContext, useState } from 'react';
+import { Fragment } from 'react/cjs/react.production.min';
+import { UserContext } from '../Auth/TwitterAuth';
 
-async function tweetPost(a) {
-  var raw = JSON.stringify({
-    text: `${a}`,
-  });
-  var requestOptions = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization:
-        'OAuth oauth_consumer_key="vU0tafpUeRqPZVWfpg0ej9cJv",oauth_token="3026985545-4Em2ZH5C8bv2sTSy7yw1b2aO9X3FznwbHleqeKr",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1640290557",oauth_nonce="Sx4UUEnk0go",oauth_version="1.0",oauth_signature="05fGfQyqriS%2FjvSE7Uf4cwcdKls%3D"',
-    },
-    body: raw,
-    redirect: 'follow',
-  };
+const TrialButton = ({ currentQuote = { content: '', author: '' } }) => {
+  const { content, author } = currentQuote;
 
-  try {
-    const response = await fetch('https://api.twitter.com/2/tweets', requestOptions);
-    if (response.ok) {
-      const json = await response.text();
-      console.log(json);
-    } else {
-      throw new Error('Hi Mate');
-    }
-  } catch (e) {
-    console.log(e);
-  }
-  // // .then(response => response.text())
-  // .then(result => console.log(result))
-  // .catch(error => console.log('error', error));
-}
+  const {
+    user: { token, username },
+  } = useContext(UserContext);
 
-const TrialButton = () => {
-  const [display, setDisplay] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState({ error: false, message: '' });
+  const [postId, setPostId] = useState('');
+  const [linkToPost, setLinkToPost] = useState(`https://twitter.com/${username}/status/${postId}`);
 
-  useEffect(() => {
-    async function tweetPost() {
-      const a = 'Hi 1';
-
-      var raw = JSON.stringify({
-        text: `${a}`,
-      });
-      var requestOptions = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization:
-            'OAuth oauth_consumer_key="vU0tafpUeRqPZVWfpg0ej9cJv",oauth_token="3026985545-4Em2ZH5C8bv2sTSy7yw1b2aO9X3FznwbHleqeKr",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1640290557",oauth_nonce="Sx4UUEnk0go",oauth_version="1.0",oauth_signature="05fGfQyqriS%2FjvSE7Uf4cwcdKls%3D"',
-        },
-        body: raw,
-        redirect: 'follow',
-        mode: 'no-cors',
-      };
-
-      try {
-        const response = await fetch('https://api.twitter.com/2/tweets', requestOptions);
+  async function tweetPost() {
+    try {
+      setIsLoading(true);
+      setTimeout(async () => {
+        const response = await fetch('/.netlify/functions/posttweet', {
+          method: 'POST',
+          body: JSON.stringify({ token, tweetMsg: `${content} -- ${author} ` }),
+        });
         if (response.ok) {
-          const json = await response.text();
-          setDisplay(json);
+          const json = await response.json();
+          if (json.data) {
+            const { id } = json.data;
+            setPostId(id);
+            setIsError({ error: false, message: '' });
+            setLinkToPost(`https://twitter.com/${username}/status/${id}`);
+            setIsLoading(false);
+          }
         } else {
           throw new Error('Hi This is an Error');
         }
-      } catch (e) {
-        console.log(e);
-      }
-      // // .then(response => response.text())
-      // .then(result => console.log(result))
-      // .catch(error => console.log('error', error));
+      }, 500);
+    } catch (e) {
+      setIsError({ error: true, message: e.message });
+      console.log(e);
     }
-  }, []);
-
-  return <button onClick={tweetPost}>MyButton</button>;
+  }
+  return (
+    <Fragment>
+      {isLoading && (
+        <div
+          style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            height: '100%',
+            width: '100%',
+            backgroundColor: 'rgba(5,5,3, 0.7',
+            display: 'grid',
+            placeContent: 'center',
+          }}
+        >
+          <progress max="100"></progress>
+          <h2 style={{ color: 'whitesmoke' }}>Loading</h2>
+        </div>
+      )}
+      {isError.error && <div>{JSON.stringify(isError.message, null, 2)}</div>}
+      <button disabled={!token} onClick={tweetPost}>
+        Post To Twitter
+      </button>
+      {postId && (
+        <a href={linkToPost} rel="noreferrer" target="_blank">
+          View Post On Twitter
+        </a>
+      )}
+    </Fragment>
+  );
 };
 
 export default TrialButton;
