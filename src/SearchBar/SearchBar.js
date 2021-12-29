94d315f79a7569f185ba71327ebd3dda976a85da
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-12-29 17:54:27 +0800

    add useCallback to function and dep for hooks

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,88 +1,63 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from 'react';
 
 const SearchBar = (props) => {
+  const [searchItem, setSearchItem] = useState('');
 
-    const [searchItem, setSearchItem] = useState('')
+  const asyncFunction = useCallback(
+    () => async () => {
+      try {
+        const response = await fetch(`https://api.quotable.io/search/quotes?query=${searchItem}&fields=content`);
 
+        if (response.ok) {
+          const json = await response.json();
 
-    const asyncFunction = async () => {
-        try {
-            const response = await fetch(`https://api.quotable.io/search/quotes?query=${searchItem}&fields=content`)
-
-
-
-            if (response.ok) {
-                const json = await response.json();
-
-                if (json.results.length === 0) {
-                    props.setLoaded(false);
-                    setSearchItem('');
-
-
-
-                } else {
-                    props.setSelectItem(json.results)
-                    props.setLoaded(true)
-                    setSearchItem('')
-                    console.log(json)
-                    console.log(typeof json.results)
-                }
-
-
-            } else {
-                throw new Error('This is an invalid request')
-            }
-
-        } catch (e) {
-            console.log(e)
+          if (json.results.length === 0) {
+            props.setLoaded(false);
+            setSearchItem('');
+          } else {
+            props.setSelectItem(json.results);
+            props.setLoaded(true);
+            setSearchItem('');
+            console.log(json);
+            console.log(typeof json.results);
+          }
+        } else {
+          throw new Error('This is an invalid request');
         }
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    [props, searchItem]
+  );
 
-    }
+  useEffect(() => {
+    asyncFunction();
+  }, [asyncFunction]);
 
-    useEffect(() => {
+  const handleInput = (e) => {
+    setSearchItem(e.target.value);
+  };
 
-        asyncFunction()
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
+    asyncFunction();
 
+    // setSearchItem('')
+  };
 
-    }, [])
+  const styles = {
+    fontSize: '1.25rem',
+    borderRadius: '5px',
+  };
 
-
-
-    const handleInput = (e) => {
-        setSearchItem(e.target.value)
-    }
-
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        asyncFunction();
-
-
-
-
-        // setSearchItem('')
-
-
-    }
-
-    const styles = {
-        fontSize: '1.25rem',
-        borderRadius: '5px'
-
-    }
-
-
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <input type='text' placeholder="Search"
-                style={styles}
-                onChange={handleInput} value={searchItem} />
-            <input type="submit" value="Submit" style={styles} />
-        </form>
-    )
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Search" style={styles} onChange={handleInput} value={searchItem} />
+      <input type="submit" value="Submit" style={styles} />
+    </form>
+  );
+};
 
 export default SearchBar;
